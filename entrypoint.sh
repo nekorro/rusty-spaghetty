c561fce5623fe577066833ef41ea79585c177f6f
Andrei Shibalov <a.shibalov@corp.mail.ru>
2025-07-08 17:57:10 +0300

    feat: set 8080 as fixed servce port

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,6 @@
 
 echo "$UPSTREAM"
 
-export PORT="${PORT:-"443"}"
-echo "$PORT"
-
 sh /conf/nginx_stream.conf.template >  /opt/nginx/stream.conf.d/nginx_stream.conf
 echo /opt/nginx/stream.conf.d/nginx_stream.conf
 cat /opt/nginx/stream.conf.d/nginx_stream.conf
